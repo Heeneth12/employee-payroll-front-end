95e64313337d60e6a804096eba9ac25951fb441a
heeneth <heeneth123@gmail.com>
2024-03-28 16:50:14 +0530

    added edit component form

--- src/app/edit-employee/edit-employee.component.ts
+++ src/app/edit-employee/edit-employee.component.ts
@@ -1,10 +1,27 @@
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-employee',
   templateUrl: './edit-employee.component.html',
-  styleUrl: './edit-employee.component.css'
+  styleUrl: './edit-employee.component.css',
 })
 export class EditEmployeeComponent {
+  constructor(private http: HttpClient, private router: Router) {}
 
+  editData = new FormGroup({
+    editEmployeeName: new FormControl(),
+    editProfileImage: new FormControl(),
+    editEmployeeGender: new FormControl(),
+    editEmployeeDepartment: new FormControl(),
+    editEmployeeSalary: new FormControl(),
+    editStartDate: new FormControl(),
+    editEmployeeNotes: new FormControl(),
+  });
+
+  showData() {
+    console.log(this.editData.value);
+  }
 }
